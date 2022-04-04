d2c16eea10eef5a80747a25c0781bfc38aea51ce
Krkmx41 <64363842+Krkmx41@users.noreply.github.com>
2022-04-04 14:01:45 +0300

    asdf

---
+++ src/Components/TenthSlide /TenthSlide.js
@@ -0,0 +1,16 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { ContainerChoose } from "../../Styles";
+import Information from "../../Store/Store"
+
+const TenthSlide = observer(() => {
+  return (
+    <ContainerChoose>
+      <h2></h2>
+      <div></div>
+      <h3 onClick={()=>{Information.toNextSlide()}}>I don't know</h3>
+    </ContainerChoose>
+  );
+});
+
+export default TenthSlide;
