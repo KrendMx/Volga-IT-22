916f11d8b9ae77c63fb61e09139196d34cf1f009
Krkmx41 <64363842+Krkmx41@users.noreply.github.com>
2022-04-08 11:21:26 +0300

    sadf

---
+++ build/precache-manifest.2f3032d840b5da568b63b277f98d1a9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "db18514ba256d068aa69",
+    "url": "/static/js/main.5481b672.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c2b630a736cbed04c3e3",
+    "url": "/static/js/2.f350b4d5.chunk.js"
+  },
+  {
+    "revision": "987fa654c2bcb6b894dd467a342b5ed2",
+    "url": "/index.html"
+  }
+];
